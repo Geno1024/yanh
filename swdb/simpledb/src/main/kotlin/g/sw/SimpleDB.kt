b3deb2a521432903d5a0230a4f8d3ffd75760de6
Y. Z. Chen <754097987@qq.com>
2025-12-20 22:48:59 +0800

    [Update] swdb/simpledb: write a dummy entry point.

---
+++ swdb/simpledb/src/main/kotlin/g/sw/SimpleDB.kt
@@ -0,0 +1,15 @@
+package g.sw
+
+/**
+ * Simple DataBase.
+ *
+ * Using [java.io.ObjectInputStream] and [java.io.ObjectOutputStream] for object IO.
+ */
+object SimpleDB
+{
+    @JvmStatic
+    fun main(args: Array<String>)
+    {
+        println("SimpleDB.")
+    }
+}
