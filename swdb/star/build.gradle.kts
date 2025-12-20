9442892ec5c69f4926ac29f2acf44e7985ab11fa
Y. Z. Chen <754097987@qq.com>
2025-12-20 20:03:01 +0800

    [Update] swdb/star: add jar build process.

--- swdb/star/build.gradle.kts
+++ swdb/star/build.gradle.kts
@@ -1,7 +1,9 @@
 import g.buildsrc.CountTask
+import java.util.jar.Attributes
 
 plugins {
     kotlin("jvm")
+    application
 }
 
 val runCountTask by tasks.register<CountTask.RunCountTask>("runCount")
@@ -13,7 +15,18 @@ tasks.withType<JavaCompile> {
 
 tasks.withType<Jar> {
     dependsOn(packCountTask)
+    manifest {
+        attributes(
+            Attributes.Name.MANIFEST_VERSION.toString() to "1.0",
+            Attributes.Name.MAIN_CLASS.toString() to application.mainClass,
+            Attributes.Name.IMPLEMENTATION_VENDOR.toString() to "Geno1024"
+        )
+    }
 }
 
 group = "g.sw.star"
 version = "0.0.${packCountTask.count}.${runCountTask.count}"
+
+application {
+    mainClass = "g.sw.Star"
+}
