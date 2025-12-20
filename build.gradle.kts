774c0917cad545f6b7aca1b9d4e5ca9c6516f4be
Y. Z. Chen <754097987@qq.com>
2025-12-20 23:44:45 +0800

    [Update] swdb/simpledb: add Dokka as doc generator.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
 plugins {
     kotlin("jvm") version "2.3.0" apply false
+    id("org.jetbrains.dokka") version "2.1.0" apply false
+    id("org.jetbrains.dokka-javadoc") version "2.1.0" apply false
 }
--- swdb/simpledb/build.gradle.kts
+++ swdb/simpledb/build.gradle.kts
@@ -4,18 +4,33 @@ import java.util.jar.Attributes
 plugins {
     kotlin("jvm")
     application
+    id("org.jetbrains.dokka")
+    id("org.jetbrains.dokka-javadoc")
 }
 
 val runCountTask by tasks.register<CountTask.RunCountTask>("runCount")
 val packCountTask by tasks.register<CountTask.PackCountTask>("packCount")
 
-tasks.withType<JavaCompile> {
+val dokkaHtmlJar by tasks.register<Jar>("dokkaHtmlJar") {
+    group = "documentation"
+    from(tasks.dokkaGeneratePublicationHtml.flatMap { it.outputDirectory })
+    archiveClassifier.set("htmldoc")
+}
+
+val dokkaJavadocJar by tasks.register<Jar>("dokkaJavadocJar") {
+    group = "documentation"
+    from(tasks.dokkaGeneratePublicationJavadoc.flatMap { it.outputDirectory })
+    archiveClassifier.set("javadoc")
+}
+
+tasks.named<JavaCompile>("compileJava") {
     dependsOn(runCountTask)
 }
 
-tasks.withType<Jar> {
+tasks.named<Jar>("jar") {
     dependsOn(packCountTask)
     dependsOn(tasks.getByName("kotlinSourcesJar"))
+    dependsOn(tasks.dokkaGenerate)
     manifest {
         attributes(
             Attributes.Name.MANIFEST_VERSION.toString() to "1.0",
@@ -23,6 +38,7 @@ tasks.withType<Jar> {
             Attributes.Name.IMPLEMENTATION_VENDOR.toString() to "Geno1024"
         )
     }
+    finalizedBy(dokkaJavadocJar, dokkaHtmlJar)
 }
 
 group = "g.sw.simpledb"
