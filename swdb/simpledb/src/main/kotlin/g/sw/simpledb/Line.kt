246dad731fd2386938c8f65daff7672e9669044c
Y. Z. Chen <754097987@qq.com>
2026-01-16 00:30:07 +0800

    [Update] swdb/simpledb: fun get complete.

--- swdb/simpledb/src/main/kotlin/g/sw/simpledb/Line.kt
+++ swdb/simpledb/src/main/kotlin/g/sw/simpledb/Line.kt
@@ -1,10 +1,14 @@
 package g.sw.simpledb
 
+import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.RandomAccessFile
+import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.jvmErasure
 
 /**
  * A [g.sw.simpledb.Line] in [g.sw.simpledb]
@@ -70,7 +74,7 @@ interface Line<T : Line<T>>
                         write(length ushr 16)
                         write(length ushr 8)
                         write(length)
-                        write(it.toByteArray())
+                        write(it.toByteArray(Charsets.UTF_8))
                     }
                 }
             }
@@ -78,6 +82,75 @@ interface Line<T : Line<T>>
 
     companion object
     {
+        fun <T : Line<T>> deser(clazz: KClass<T>, bais: ByteArrayInputStream): T = clazz.primaryConstructor!!
+            .parameters
+            .withIndex()
+            .sortedBy { it.value.findAnnotation<Sequence>()?.value ?: (it.index * 10) }
+            .associate { (_, parameter) ->
+                parameter to when (parameter.type.jvmErasure)
+                {
+                    Byte::class -> {
+                        bais.read().toByte()
+                    }
+                    Short::class -> {
+                        (
+                            (bais.read() shl 8) +
+                                bais.read()
+                            ).toShort()
+                    }
+                    Int::class -> {
+                        (bais.read() shl 24) +
+                            (bais.read() shl 16) +
+                            (bais.read() shl 8) +
+                            bais.read()
+                    }
+                    Long::class -> {
+                        (
+                            (bais.read().toLong() shl 56) +
+                                (bais.read().toLong() shl 48) +
+                                (bais.read().toLong() shl 40) +
+                                (bais.read().toLong() shl 32) +
+                                (bais.read() shl 24) +
+                                (bais.read() shl 16) +
+                                (bais.read() shl 8) +
+                                (bais.read())
+                            )
+                    }
+                    Float::class -> {
+                        Float.fromBits(
+                            (bais.read() shl 24) +
+                                (bais.read() shl 16) +
+                                (bais.read() shl 8) +
+                                bais.read()
+                        )
+                    }
+                    Double::class -> {
+                        Double.fromBits(
+                            (bais.read().toLong() shl 56) +
+                                (bais.read().toLong() shl 48) +
+                                (bais.read().toLong() shl 40) +
+                                (bais.read().toLong() shl 32) +
+                                (bais.read() shl 24) +
+                                (bais.read() shl 16) +
+                                (bais.read() shl 8) +
+                                (bais.read())
+                        )
+                    }
+                    String::class -> {
+                        val length = (bais.read() shl 24) + (bais.read() shl 16) + (bais.read() shl 8) + bais.read()
+                        val buf = ByteArray(length)
+                        bais.read(buf)
+                        buf.toString(Charsets.UTF_8)
+                    }
+                    else -> {
+                    }
+                }
+            }.let {
+                clazz.primaryConstructor!!.callBy(it)
+            }
+
+        inline fun <reified T : Line<T>> deser(bais: ByteArrayInputStream): T = deser(T::class, bais)
+
         fun <T : Line<T>> read(raf: RandomAccessFile): Line<T>
         {
             return object : Line<T>{}
--- swdb/simpledb/src/main/kotlin/g/sw/simpledb/Table.kt
+++ swdb/simpledb/src/main/kotlin/g/sw/simpledb/Table.kt
@@ -1,5 +1,7 @@
 package g.sw.simpledb
 
+import java.io.ByteArrayInputStream
+import java.io.File
 import java.io.RandomAccessFile
 import kotlin.reflect.KClass
 
@@ -20,29 +22,72 @@ class Table<T: Line<T>>(val lineDef: Class<T>, val name: String)
 {
     constructor(lineDef: KClass<T>, name: String): this(lineDef.java, name)
 
-    private val metadata = RandomAccessFile("$name.gsdb", "rw")
-    private val index = RandomAccessFile("$name.gsdbi", "rw")
-    private val pool = RandomAccessFile("$name.gsdbp", "rw")
+    private val metadataFile = RandomAccessFile("$name.gsdb", "rw")
+    private var indexFile = RandomAccessFile("$name.gsdbi", "rw")
+    private var poolFile = RandomAccessFile("$name.gsdbp", "rw")
 
     /**
      * Create empty table.
      */
     fun init(): Table<T> = apply {
-        metadata.write("gsdb0001".toByteArray())
-        metadata.write(System.currentTimeMillis().toHexString(HexFormat.Default).padStart(8, '0').hexToByteArray())
+        metadataFile.write("gsdb0001".toByteArray())
+        metadataFile.write(System.currentTimeMillis().toHexString(HexFormat.Default).padStart(8, '0').hexToByteArray())
         val qn = lineDef.canonicalName.toString().toByteArray()
-        metadata.write(qn.size.toHexString().padStart(4, '0').hexToByteArray())
-        metadata.write(qn)
-        index.setLength(0)
-        pool.setLength(0)
+        metadataFile.write(qn.size.toHexString().padStart(4, '0').hexToByteArray())
+        metadataFile.write(qn)
+        indexFile.setLength(0)
+        poolFile.setLength(0)
     }
 
-    fun add(line: T)
+    fun add(line: T) = apply {
+        indexFile.seek(indexFile.length())
+        poolFile.seek(poolFile.length())
+        indexFile.writeLong(poolFile.filePointer)
+        val ser = line.ser()
+        poolFile.writeInt(ser.size)
+        poolFile.write(ser)
+    }
+
+    fun remove(vararg index: Long) = apply {
+        val tempIndexFile = RandomAccessFile("$name.gsdbit", "rw")
+        val tempPoolFile = RandomAccessFile("$name.gsdbpt", "rw")
+        LongRange(0, indexFile.length() / 8)
+            .filterNot { it !in index }
+            .forEach {
+                tempIndexFile.writeLong(tempPoolFile.filePointer)
+                indexFile.seek(it * 8)
+                val thiz = indexFile.readLong()
+                tempPoolFile.seek(thiz)
+                poolFile.seek(thiz)
+                val size = poolFile.readInt()
+                val buffer = ByteArray(size)
+                poolFile.read(buffer)
+                tempPoolFile.writeInt(size)
+                tempPoolFile.write(buffer)
+            }
+        tempIndexFile.close()
+        tempPoolFile.close()
+        indexFile.close()
+        poolFile.close()
+        File("$name.gsdbit").renameTo(File("$name.gsdbi"))
+        File("$name.gsdbpt").renameTo(File("$name.gsdbp"))
+        indexFile = RandomAccessFile("$name.gsdbi", "rw")
+        poolFile = RandomAccessFile("$name.gsdbp", "rw")
+    }
+
+    operator fun get(index: Long): T
     {
-        index.seek(index.length())
-        pool.seek(pool.length())
-        index.writeLong(pool.filePointer)
-        pool.write(line.ser())
+        indexFile.seek(index * 8)
+        val thiz = indexFile.readLong()
+        poolFile.seek(thiz)
+        val buffer = ByteArray(poolFile.readInt())
+        poolFile.read(buffer)
+        return Line.deser(lineDef.kotlin, ByteArrayInputStream(buffer))
+    }
+
+    operator fun plusAssign(line: T)
+    {
+        add(line)
     }
 
 }
