fb179070a0157412f9fabbefa07ed7d42271cc90
Y. Z. Chen <754097987@qq.com>
2026-01-24 23:13:54 +0800

    [Pipeline] try to publish Kover report to GitHub Pages.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,9 @@ plugins {
     kotlin("jvm") version "2.3.0" apply false
     id("org.jetbrains.dokka") version "2.1.0" apply false
     id("org.jetbrains.dokka-javadoc") version "2.1.0" apply false
-    id("org.jetbrains.kotlinx.kover") version "0.9.4" apply false
+    id("org.jetbrains.kotlinx.kover") version "0.9.4"
+}
+
+dependencies {
+    kover(project(":swdb:simpledb"))
 }
