2556e2318f27e224a1b89baf456716744c58b311
Y. Z. Chen <754097987@qq.com>
2026-01-25 00:35:16 +0800

    [Pipeline] aggregate test reports.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,16 @@
 plugins {
-    kotlin("jvm") version "2.3.0" apply false
+    kotlin("jvm") version "2.3.0"
     id("org.jetbrains.dokka") version "2.1.0" apply false
     id("org.jetbrains.dokka-javadoc") version "2.1.0" apply false
     id("org.jetbrains.kotlinx.kover") version "0.9.4"
+    id("test-report-aggregation")
 }
 
 dependencies {
     kover(project(":swdb:simpledb"))
+    testReportAggregation(project(":swdb:simpledb"))
+}
+
+tasks.check {
+    dependsOn(tasks.named<TestReport>("testAggregateTestReport"))
 }
