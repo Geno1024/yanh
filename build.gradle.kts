bcb41011c6ad490a1137a7bb631f0694fb893e71
Y. Z. Chen <754097987@qq.com>
2026-01-25 10:17:17 +0800

    [Pipeline] generate dokka docs.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,20 @@
 plugins {
     kotlin("jvm") version "2.3.0"
-    id("org.jetbrains.dokka") version "2.1.0" apply false
-    id("org.jetbrains.dokka-javadoc") version "2.1.0" apply false
+    id("org.jetbrains.dokka") version "2.1.0"
+    id("org.jetbrains.dokka-javadoc") version "2.1.0"
     id("org.jetbrains.kotlinx.kover") version "0.9.4"
     id("test-report-aggregation")
 }
 
 dependencies {
-    kover(project(":swdb:simpledb"))
-    testReportAggregation(project(":swdb:simpledb"))
+    listOf(
+        project(":swdb:simpledb"),
+        project(":swdb:star")
+    ).forEach {
+        dokka(it)
+        kover(it)
+        testReportAggregation(it)
+    }
 }
 
 tasks.check {
