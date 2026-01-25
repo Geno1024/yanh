32437de587b9623fc8ec801ec8ca50d47db5e6ff
Y. Z. Chen <754097987@qq.com>
2026-01-25 19:11:54 +0800

    [Pipeline] use Gradle to generate download page.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,3 +20,31 @@ dependencies {
 tasks.check {
     dependsOn(tasks.named<TestReport>("testAggregateTestReport"))
 }
+
+tasks.register<Exec>("generateDownloadPagesForGitHubPages") {
+    doLast {
+        File("$rootDir/pages/download.html").writeText("""
+            <!doctype html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>Yanh :: Downloads</title>
+            </head>
+            <body>
+            <table style="width: 100%">
+                <thead>
+                    <tr><th>Name</th><th>Size</th><th>Version</th></tr>
+                </thead>
+                <tbody>
+                    ${File("./pages/full").listFiles().map {
+                        "<tr><td>$it</td><td></td><td></td>"
+                    }}
+                </tbody>
+            </table>
+            </body>
+            </html>
+
+        """.trimMargin())
+    }
+}
