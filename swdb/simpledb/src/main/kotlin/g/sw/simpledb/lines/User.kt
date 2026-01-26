1076f2658e677aa3b5fe4799a9424a981758d0d7
Y. Z. Chen <754097987@qq.com>
2026-01-26 21:42:28 +0800

    [Update] swdb/simpledb: user table confirmed.

--- swdb/simpledb/src/main/kotlin/g/sw/simpledb/lines/User.kt
+++ swdb/simpledb/src/main/kotlin/g/sw/simpledb/lines/User.kt
@@ -4,6 +4,14 @@ import g.sw.simpledb.Line
 import g.sw.simpledb.Sequence
 
 data class User(
+    @property:Sequence(0) var id: Int,
     @property:Sequence(10) var login: String,
     @property:Sequence(20) var name: String,
+    @property:Sequence(30) var password: String,
+    @property:Sequence(40) var passkey: String,
+    @property:Sequence(50) var gender: String,
+    @property:Sequence(60) var birthday: String,
+    @property:Sequence(70) var type: String,
+    @property:Sequence(80) var role: String,
+    @property:Sequence(90) var registerTime: Long
 ) : Line<User>
